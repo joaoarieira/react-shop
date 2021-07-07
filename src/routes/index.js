6cc91ed388ca64af9110c8bb8474e189955ce060
João Azevedo <joao.azevedo@ufv.br>
2021-07-07 15:27:08 -0300

    Redirect user after sign in.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
 import ShopPage from '../pages/ShopPage';
 import SignInAndSignUp from '../pages/SignInAndSignUp';
 
 function Routes(props) {
+    const currentUser = useSelector(state => state.user.currentUser);
     return (
         <Switch>
             <Route path="/" exact component={HomePage}></Route>
             <Route path="/shop" component={ShopPage}></Route>
-            <Route path="/signin" component={SignInAndSignUp}></Route>
+            <Route path="/signin" exact>
+                {currentUser ? <Redirect to='/' /> : <SignInAndSignUp />}
+            </Route>
         </Switch>
     );
 }
